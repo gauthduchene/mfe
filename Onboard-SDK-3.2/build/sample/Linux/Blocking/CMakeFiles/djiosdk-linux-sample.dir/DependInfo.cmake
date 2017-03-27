68be140edbb601704685dfb76c9fe572cdcc48f3
Gauthier Rasp <gauthierduchene@gmail.com>
2017-03-27 10:39:14 +0200

    build

--- Onboard-SDK-3.2/build/sample/Linux/Blocking/CMakeFiles/djiosdk-linux-sample.dir/DependInfo.cmake
+++ Onboard-SDK-3.2/build/sample/Linux/Blocking/CMakeFiles/djiosdk-linux-sample.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/duduche/Documents/mfe/Onboard-SDK-3.2/sample/Linux/Blocking/src/LinuxInteractive.cpp" "/home/duduche/Documents/mfe/Onboard-SDK-3.2/build/sample/Linux/Blocking/CMakeFiles/djiosdk-linux-sample.dir/src/LinuxInteractive.cpp.o"
-  "/home/duduche/Documents/mfe/Onboard-SDK-3.2/sample/Linux/Blocking/src/main.cpp" "/home/duduche/Documents/mfe/Onboard-SDK-3.2/build/sample/Linux/Blocking/CMakeFiles/djiosdk-linux-sample.dir/src/main.cpp.o"
-  "/home/duduche/Documents/mfe/Onboard-SDK-3.2/sample/Linux/Blocking/src/thread.cpp" "/home/duduche/Documents/mfe/Onboard-SDK-3.2/build/sample/Linux/Blocking/CMakeFiles/djiosdk-linux-sample.dir/src/thread.cpp.o"
+  "/home/pi/Documents/mfe/Onboard-SDK-3.2/sample/Linux/Blocking/src/LinuxInteractive.cpp" "/home/pi/Documents/mfe/Onboard-SDK-3.2/build/sample/Linux/Blocking/CMakeFiles/djiosdk-linux-sample.dir/src/LinuxInteractive.cpp.o"
+  "/home/pi/Documents/mfe/Onboard-SDK-3.2/sample/Linux/Blocking/src/main.cpp" "/home/pi/Documents/mfe/Onboard-SDK-3.2/build/sample/Linux/Blocking/CMakeFiles/djiosdk-linux-sample.dir/src/main.cpp.o"
+  "/home/pi/Documents/mfe/Onboard-SDK-3.2/sample/Linux/Blocking/src/thread.cpp" "/home/pi/Documents/mfe/Onboard-SDK-3.2/build/sample/Linux/Blocking/CMakeFiles/djiosdk-linux-sample.dir/src/thread.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
@@ -21,9 +21,9 @@ set(CMAKE_CXX_TARGET_INCLUDE_PATH
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/home/duduche/Documents/mfe/Onboard-SDK-3.2/build/osdk-core/CMakeFiles/djiosdk-core.dir/DependInfo.cmake"
-  "/home/duduche/Documents/mfe/Onboard-SDK-3.2/build/platform/linux/CMakeFiles/djiosdk-platform.dir/DependInfo.cmake"
-  "/home/duduche/Documents/mfe/Onboard-SDK-3.2/build/osdk-wrapper/CMakeFiles/djiosdk-wrapper.dir/DependInfo.cmake"
+  "/home/pi/Documents/mfe/Onboard-SDK-3.2/build/osdk-core/CMakeFiles/djiosdk-core.dir/DependInfo.cmake"
+  "/home/pi/Documents/mfe/Onboard-SDK-3.2/build/platform/linux/CMakeFiles/djiosdk-platform.dir/DependInfo.cmake"
+  "/home/pi/Documents/mfe/Onboard-SDK-3.2/build/osdk-wrapper/CMakeFiles/djiosdk-wrapper.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
